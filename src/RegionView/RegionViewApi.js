734f9615683cfb1838632318c9520f18d58a3aa8
naufalfirdaus <naufalfirdausa@gmail.com>
2023-03-24 10:13:50 +0700

    Day03

--- src/RegionView/RegionViewApi.js
+++ src/RegionView/RegionViewApi.js
@@ -1,42 +1,63 @@
 import React, { useEffect, useState } from 'react'
 import RegionApi from '../api/RegionApi'
+import RegionCreateForm from './RegionCreateForm'
+import RegionEditForm from './RegionEditForm'
 
 export default function RegionViewApi() {
     const [region, setRegion] = useState([])
-    const [refresh,setRefresh] = useState(false)
+    const [refresh, setRefresh] = useState(false)
+    const [display, setDisplay] = useState(false)
+    const [displayEdit, setDisplayEdit] = useState(false)
+    const [id, setId] = useState()
     useEffect(() => {
         RegionApi.list().then(data => {
             setRegion(data)
         })
         setRefresh(false)
     }, [refresh])
-    const onDelete = async(id) => {
-        RegionApi.deleted(id).then(()=> {
+    const onDelete = async (id) => {
+        RegionApi.deleted(id).then(() => {
             window.alert('Data Successfully Delete')
             setRefresh(true)
         })
     }
+    const onClick = (id) => {
+        setDisplayEdit(true)
+        setId(id)
+    }
     return (
         <div>
-            <h2>List Region</h2>
-            <table>
-                <th>Region ID</th>
-                <th>Region Name</th>
-                <th>Action</th>
-                <tbody>
-                    {
-                        region && region.map(reg => (
-                            <tr key={reg.regionId}>
-                                <td>{reg.regionId}</td>
-                                <td>{reg.regionName}</td>
-                                <td>
-                                    <button onClick={()=>onDelete(reg.regionId)}> Delete Region </button>
-                                </td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+            {displayEdit ? <RegionEditForm
+                id={id}
+                setRefresh={setRefresh} /> :
+                display ? <RegionCreateForm
+                    setRefresh={setRefresh}
+                    setDisplay={setDisplay}
+                /> :
+                    <>
+                        <h2>List Region</h2>
+                        <button onClick={() => setDisplay(true)}>Add region</button>
+                        <table>
+                            <th>Region ID</th>
+                            <th>Region Name</th>
+                            <th>Action</th>
+                            <tbody>
+                                {
+                                    region && region.map(reg => (
+                                        <tr key={reg.regionId}>
+                                            <td>{reg.regionId}</td>
+                                            <td>{reg.regionName}</td>
+                                            <td>
+                                                <button onClick={() => onDelete(reg.regionId)}> Delete Region </button>
+                                                <button onClick={() => onClick(reg.regionId)}> Edit Region </button>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </>
+            }
         </div>
     )
 }
--- src/api/RegionApi.js
+++ src/api/RegionApi.js
@@ -18,4 +18,30 @@ const deleted = async (id) => {
     }
 }
 
-export default { list, deleted }
+const create = async (payload) => {
+    try {
+        const result = await axios.post(`${config.domain}/region/`, payload)
+        return result
+    } catch (error) {
+        return await error.message
+    }
+}
+
+const update = async (data) => {
+    try {
+        const result = await axios.put(`${config.domain}/region/${data.id}`, data)
+        return result
+    } catch (error) {
+        return await error.message
+    }
+}
+const findOne = async (id) => {
+    try {
+        const result = await axios.get(`${config.domain}/region/${id}`)
+        return result.data
+    } catch (error) {
+        return await error.message
+    }
+}
+
+export default { list, deleted, create, update, findOne }
